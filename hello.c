6eb996a6260336b22088f3a54d52975d1d652a36
SaurabhKolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-05 14:26:36 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+'''
+Flow/Syntax: 1.Semicolon(;)  is not use in python.
+             2.Indentation: We give some spaces or tab new line after for(),while(),if(),etc.
+             3.declaration is not available in python.We cannot do like a instead we do like a=0 or a=None or a=" "
+             
+Comments in Python: 1.Single line comment: # is use for Single line comment.
+                    2.Multi line comment: ''' ...comments..... ''' is use for multiline comment
+'''
+
+#Simple Hello world program
+
+print("Hello World") 
+#We have to just write in print,no main function or any class is needed.So it is very easy language.  
