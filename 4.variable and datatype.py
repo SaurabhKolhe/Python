fc83a95e12b728aa1140d2f18c41643c1698e50f
SaurabhKolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-05 14:41:42 +0530

    Create 4.variable and datatype.py

---
+++ 4.variable and datatype.py
@@ -0,0 +1,25 @@
+'''
+Variable: It is a container that hold value.
+
+Datatype: Type of data which we want to store in container or variable
+          There are 14 datatypes:
+          Fundamental                  Collection              Other
+          1.int (Integer)              6.List                  10.Byte
+          2.float                      7.Touple                11.ByteArray  
+          3.str  (string)              8.Dictionary            12.Frozenset 
+          4.bool (boolean)             9.Set                   13.None
+          5.complex (complex)                                  14.Range 
+          
+         double,character not supported.
+         Array is not supported.
+         curly braces {} is not supported.
+'''         
+a=10
+b='Hello'
+c=10.39
+d=4+3j
+e=True
+f=False
+
+#we can use following command in terminal to check type of variable:
+type(variable_name)
