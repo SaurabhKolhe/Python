7055c8308efd44a2fcccbed5fab141f4e3e2a067
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-06 14:55:21 +0530

    Create 5typecasting.py

---
+++ 5typecasting.py
@@ -0,0 +1,53 @@
+#We can convert one datatype to other by typecasting
+
+#To convert into integer
+a='1'        #string
+a=int(a)
+print(type(a))
+
+b=5.2         #float
+b=int(b)
+print(type(b))
+
+c=True       #boolean
+c=int(c)
+print(type(c))
+
+#To convert into float
+x=1        #integer
+x=float(x)
+print(type(x))
+
+y=True     #boolean
+y=float(y)
+print(type(y))
+
+z="2"       #string
+z=float(z)
+print(type(z))
+
+#To convert into string
+l=10         #integer
+l=str(l)
+print(type(l))
+
+m=10.5      #float
+m=str(m)
+print(type(m))
+
+n=True       #boolean
+n=str(n)
+print(type(n))
+
+#To Convert into boolean 
+o="1"       #string
+o=bool(o)
+print(type(o))
+
+p=0      #integer
+p=bool(p)
+print(type(p))
+
+q=1.0      #float
+q=bool(q)
+print(type(q))
