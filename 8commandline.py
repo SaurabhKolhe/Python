f48f10224f2c20792dc861db336468456d9e3947
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-06 15:12:11 +0530

    Create 8commandline.py

---
+++ 8commandline.py
@@ -0,0 +1,11 @@
+#Command-Line argument:
+                       We take input through command line i.e with run command on terminal (python file.py input1 input2)
+                      
+import sys
+a=int(sys.argv[1]) 
+b=int(sys.argv[2])
+print("addition of two numbers is",a+b)
+print("subtraction of two numbers is",a-b)
+print("addition of two numbers is",a*b)
+print("Division of two numbers is",a/b)
+                      
