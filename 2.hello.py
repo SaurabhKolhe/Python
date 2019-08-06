25c975144ce6c3ef779bfd778214d80c8b42a2d2
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-06 10:20:31 +0530

    Update 2.hello.py

--- 2.hello.py
+++ 2.hello.py
@@ -10,4 +10,17 @@ Comments in Python: 1.Single line comment: # is use for Single line comment.
 #Simple Hello world program
 
 print("Hello World") 
-#We have to just write in print,no main function or any class is needed.So it is very easy language.  
+#We have to just write in print,no main function or any class is needed.So it is very easy language.
+
+#By default print having end="\n" by this we go on new line after print
+print("hello",end="\n")
+#we can write this also
+print("Hello",end=" ")
+print("I am Saurabh",end="\t")
+print("Thank You")
+
+#There is sep function 
+a=23
+b=40
+print(a,b,sep="+")
+print(a,b,sep="%")
