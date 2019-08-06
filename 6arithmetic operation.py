ddb472543b8d910135c725aa7592a7d37e2ee910
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-06 15:07:08 +0530

    Create 6arithmetic operation.py

---
+++ 6arithmetic operation.py
@@ -0,0 +1,30 @@
+#Addition
+x=int(input("Enter 1st number"))
+y=int(input("Enter 2nd number"))
+print(x+y)
+
+#Substraction
+p=int(input("Enter 1st number"))
+q=int(input("Enter 2nd number"))
+print(p-q)
+
+#Multiplication
+l=int(input("Enter 1st number"))
+m=int(input("Enter 2nd number"))
+print(l*m)
+
+#Division
+a=int(input("Enter 1st number"))
+b=int(input("Enter 2nd number"))
+print(a/b)  #float
+print(a//b) #integer
+
+#Mod or Remainder
+o=int(input("Enter 1st number"))
+p=int(input("Enter 2nd number"))
+print(o%p)
+
+#raise to power
+c=int(input("Enter 1st number"))
+d=int(input("Enter 2nd number"))
+print(c**d)   #2**3=2*2*2
