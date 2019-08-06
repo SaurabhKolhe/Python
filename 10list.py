1d1076bfafe5de00282938e0113002c06612c17f
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-06 18:42:56 +0530

    Create 10list.py

---
+++ 10list.py
@@ -0,0 +1,15 @@
+'''
+List : It contain collection data which having different datatype(can contain same datatype)
+       i.e we can add number,string,float,boolean,complex in list.
+       As array only contain data of same datatype,it is more useful.
+       List is mutable i.e we can change any specific value in list.
+       
+       Syntax:
+              list_name=[...data seperated with comma...]
+              eg: a=[10,"String",10.45,True,False,1+4j]
+              
+       Indexing: List having same indexing as string i.e positive and negative both.
+                   index           0     1       2     3    4    5      
+                                a=[10,"String",10.45,True,False,1+4j]
+                   index          -6    -5      -4    -3   -2    -1  
+                   
