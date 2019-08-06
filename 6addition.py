b3f6ee0c7e9a0877b5b3bd1b2a175dac9532aab3
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-06 15:01:01 +0530

    Create 6addition.py

---
+++ 6addition.py
@@ -0,0 +1,13 @@
+print("enter the 1st nnumber")
+a=input()
+a=int(a)     #As value in input function is store as string we have to convert it into integer
+print("enter the 2nd number")
+b=input()
+b=int(b)
+print(a+b)
+
+#We can simplifies above program as:
+x=int(input("Enter 1st number"))
+y=int(input("Enter 2nd number"))
+print(a+b)
+
