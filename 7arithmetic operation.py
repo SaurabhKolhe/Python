11be82a4f4234bd3542a0844d27b528fd3fc9b80
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-06 15:14:55 +0530

    Update 7arithmetic operation.py

--- 7arithmetic operation.py
+++ 7arithmetic operation.py
@@ -1,30 +1,30 @@
 #Addition
 x=int(input("Enter 1st number"))
 y=int(input("Enter 2nd number"))
-print(x+y)
+print("Addition:",x+y)
 
 #Substraction
 p=int(input("Enter 1st number"))
 q=int(input("Enter 2nd number"))
-print(p-q)
+print("Substraction:",p-q)
 
 #Multiplication
 l=int(input("Enter 1st number"))
 m=int(input("Enter 2nd number"))
-print(l*m)
+print("Multiplication:",l*m)
 
 #Division
 a=int(input("Enter 1st number"))
 b=int(input("Enter 2nd number"))
-print(a/b)  #float
-print(a//b) #integer
+print("Division in float:",a/b)  #float
+print("Division in integer:",a//b) #integer
 
 #Mod or Remainder
 o=int(input("Enter 1st number"))
 p=int(input("Enter 2nd number"))
-print(o%p)
+print("Remainder:",o%p)
 
 #raise to power
-c=int(input("Enter 1st number"))
-d=int(input("Enter 2nd number"))
-print(c**d)   #2**3=2*2*2
+c=int(input("Enter number"))
+d=int(input("Entee power"))
+print("Power of number :",c**d)   #2**3=2*2*2
