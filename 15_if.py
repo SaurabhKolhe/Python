0e46c3d87b6b16f7832b1131421c451a4aadb7fc
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-08-08 12:12:10 +0530

    Update 15_if.py

--- 15_if.py
+++ 15_if.py
@@ -1,1 +1,5 @@
-#Program to check 
+#Program to check value is between 10 and 100
+a=int("Enter value greater than 10 and less than 100")
+if a>10 and a<100:
+    print("You enter right number")
+    
