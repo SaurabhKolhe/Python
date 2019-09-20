ea7803a45bfbf992159efd826a91b285d8172a91
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-09-20 23:01:20 +0530

    Update 22_Exercise1.py

--- 22_Exercise1.py
+++ 22_Exercise1.py
@@ -1,16 +1,21 @@
 #Username and Password Validation
-'''user=input("Enter Username\t")
+user=input("Enter Username\t")
 passw=input("Enter Password\t")
     if user=="admin" and passw=="admin":
         print("Welcome!!!")
     else:
         print("Incorrect Username or Password")
-'''
 
 #Validation of password,mobile number and username
-user=input("Enter Username\t")
-passw=input("Enter Password\t")
-cpass=input("Confirm Password\t")
-if passw!=cpass:
+user1=input("Enter Username\t")
+if len(user)<4:
+    print("Please enter Username greater than 4 character")
+passw1=input("Enter Password\t")
+cpass1=input("Confirm Password\t")
+if passw1!=cpass1:
     print("Password not matched")
+mob=input("Enter mobile number")
+if len(mob)!=10:
+    print("Mobile Should be 10 digit")
+mob=int(mob) 
 
