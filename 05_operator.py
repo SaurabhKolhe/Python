710463b305d438b54bf05972cf45430568af9287
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-09-21 19:57:26 +0530

    Update 05_operator.py

--- 05_operator.py
+++ 05_operator.py
@@ -43,6 +43,20 @@ Identity Operator:
 1.is
 2.is not
 
+a=10
+b=10
+if a is b:
+    print("Same")
+else:
+    print("Not same")
+
+c=10
+d=20
+if c is not d:
+    print("Not same")
+else:
+    print("Same")
+
 Membership Operator:
 1.in
 2.not in
