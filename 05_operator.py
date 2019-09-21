68ed1d5a181f3f2381b8da6c8a0173ffa5677d77
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-09-21 19:55:44 +0530

    Update and rename 06_operator.py to 05_operator.py

---
+++ 05_operator.py
@@ -0,0 +1,49 @@
+'''
+Arithmetic Operator:
+1.Addition          +
+2.Substraction      -
+3.Multiplication    *
+4.Division          /  (gives answer in float eg. 2/2=1.0)
+                    // (gives answer in integer eg.2//2=1)
+5.mod               %
+6.power to          ** (gives power of number eg.2*3=8)
+
+Conditional Expressions:
+                          Meaning Math                  Symbol	  Python Symbols
+                          Less than	                     <	      <
+                          Greater than	                 >	      >
+                          Less than or equal	           ≤	      <=
+                          Greater than or equal	         ≥	      >=
+                          Assignment(Equals)	           =	      ==
+                          Not equal	                     ≠	      !=
+                          
+logical operator:
+1.and  
+      p   q    result
+      T   T      T
+      T   F      T
+      F   T      T
+      F   F      F
+      
+
+1.or  
+      p   q    result
+      T   T      T
+      T   F      F
+      F   T      F
+      F   F      F
+            
+
+1.not  (!)  
+      p     result
+      T       T
+      T       T     
+      
+Identity Operator:
+1.is
+2.is not
+
+Membership Operator:
+1.in
+2.not in
+'''
