3a2fb6a99f48098baa7a434c183adb79b287fab0
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-09-21 20:07:29 +0530

    Update and rename fstring.py to 05.operator1.py

--- 05.operator1.py
+++ 05.operator1.py
@@ -1,5 +1,5 @@
 '''
-Formatting of string:
+1.Formatting of string:
                      1.format
                      2.f
 '''
@@ -23,3 +23,18 @@ print("Value of A:{} B:{} A:{} A:{} B:{} B:{}".format(a,b,a,a,b,b))
 #Other way to write that type of program is using f"":
 print(f"Value of A:{a} B:{b} A:{a} A:{a} B:{b}")
 #It is more easier than 'format'
+
+'''
+2.doc string:
+           It is use to print comment write in first line of function.
+           It is mainly use for get information about function where we give in comment
+           Syntax:
+                  def fun_name():
+                      #........comment about function.........
+                      ..........code....................
+                  print(fun_name.__doc__)    #we use 2 times '_' before after doc
+'''
+def fun():
+    '''This function is used to known about doc string'''
+    print("Hii")
+print(fun.__doc__)
