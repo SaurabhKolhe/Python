258c0b25b01245e70b1d4de196cf8aacd61a5b13
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-09-21 20:15:50 +0530

    Update and rename math.py to 24_math.py

---
+++ 24_math.py
@@ -0,0 +1,9 @@
+#In this we will see import function 
+import math as mt     #importing math to solve complex mathematical operation
+import random as rd   #imporing rand to select value randmoly
+
+a=mt.asinh(90)
+print(a)
+
+b=rd.randint(100000,900000)
+print(b)
