99b7dfeaa53eb693a64a29dc3857adb2eac4c1d3
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-11-09 20:14:16 +0530

    Add files via upload

---
+++ 33aceesSpecifier.py
@@ -0,0 +1,17 @@
+'''
+Access Specifier:
+                 Name Mangling (Give name with suitable meaning or name which gives easy idea about behaviour)
+                 not keyword but we use it only for understanding behaviour of variables.
+
+                 Public:
+                        ByDefault
+                        All data are available for everyone.
+                        name="ABC"    #Name Mangling
+                 Protected:
+                         _name="ABC"     #Name Mangling
+                         Data limited upto that class or file
+                         Inherited class can access this data
+                 Private:
+                         __name="ABC"    #Name Mangling
+                         Data limited upto that class or file
+,,,
