26402a20030174aa9d5d30d1583f68782c770049
Saurabh Kolhe <50330087+SaurabhKolhe@users.noreply.github.com>
2019-11-09 20:29:27 +0530

    Update 37_multiple Inheritance.py

--- 37_multiple Inheritance.py
+++ 37_multiple Inheritance.py
@@ -1,4 +1,8 @@
-'''
+
+# in multiple inhertiance the priority is given to first  and
+#   at a one time one constructor is called
+# super.__init__(): we can call parent constructor
+
 class A:
     def __init__(self):
       print("hi")
@@ -41,25 +45,7 @@ class C(A,B):
     def put(self):
         print("hello b")
 c = C()
-'''
 
-# in multiple inhertiance the priority is given to first  and
-#   at a one time one constructor is called
-# super.__init__(): we can call parent constructor
 
-class A:
-    name=None
-    id=None
-    def __init__(self):
-        self.name = "ABC"
-        self.id = 123
-    def showData(self):
-        print("Name is",self.name)
-        print("Id is",self.id)
-    def updateInfo(self,name,id):
-        self.name=name
-        self.id=id
-a=A()
-a.showData()
-a.updateInfo("PQR",456)
-a.showData()
+
+
